65855d1f6bf1f5d72848c167023364865b922612
Yanda Huang <ward11sdo@qq.com>
2018-04-23 13:19:12 -0400

    Fix rectangle, fix unit, add integration

--- attempt2.py
+++ attempt2.py
@@ -1,9 +1,9 @@
 import numpy as np
-import pdb
 import cv2 as cv
 import os
 import Cali_Cam as cc
-import json
+import requests
+import pprint
 
 
 TEST_IMGS_PATH = 'test_imgs/'
@@ -13,9 +13,10 @@ small_arc_lower = 0.83
 bone_upper = 0.83
 bone_lower = 0.75
 big_arc_upper = 0.75
-big_arc_lower = 0.65 
+big_arc_lower = 0.65
 goal_upper = 0.65
 padding = 50
+
 
 class Obstacle:
     def __init__(self, contour, area, moments, x_scale, y_scale, upper_left, z):
@@ -30,12 +31,13 @@ class Obstacle:
         self.x_scale = x_scale
         self.y_scale = y_scale
         self.cbx = upper_left[0]
-        self.cby = upper_left[1] 
+        self.cby = upper_left[1]
         self.z = z
         self.inferType()
 
         # if negative angle, rotate wrist counter clockwise
-        self.angle = self.rect[2] if self.rect[2] > -45. else self.rect[2] + 90.  # just trust it 
+        self.angle = self.rect[2] if self.rect[2] > - \
+            45. else self.rect[2] + 90.  # just trust it
 
     def inferType(self):
         if self.solidity > rect_lower:
@@ -47,19 +49,25 @@ class Obstacle:
         elif self.solidity < bone_upper and self.solidity > bone_lower:
             self.type = 'bone'
         elif self.solidity < small_arc_upper and self.solidity > small_arc_lower:
-            self.type = 'small arc' 
+            self.type = 'small arc'
         else:
             self.type = 'WTF IS THIS?!?!?!?!'
-    
+
     def visualize(self, pic):
         box = cv.boxPoints(self.rect)
         box = np.int0(box)
-        cv.drawContours(pic,[box],0,(0,0,255),2)
-        (x,y,w,h) = self.boundingRect
-        cv.putText(pic, self.type, (x+w,y+h), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv.LINE_AA)  # red text
-        cv.putText(pic, str(self.angle),(x,y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv.LINE_AA)  # red text
-        centroid = '(' + str(int((self.cx-self.cbx)*self.x_scale)) + ', ' + str(int((self.cy-self.cby)*self.y_scale))+ ', ' + str(int(self.z)) + ')'
-        cv.putText(pic, centroid, (x+20,y+20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1, cv.LINE_AA)  # red text
+        cv.drawContours(pic, [box], 0, (0, 0, 255), 2)
+        (x, y, w, h) = self.boundingRect
+        cv.putText(pic, self.type, (x+w, y+h), cv.FONT_HERSHEY_SIMPLEX,
+                   0.5, (0, 0, 255), 1, cv.LINE_AA)  # red text
+        cv.putText(pic, str(self.angle), (x, y), cv.FONT_HERSHEY_SIMPLEX,
+                   0.5, (0, 255, 0), 1, cv.LINE_AA)  # red text
+        centroid = '(' + str(int((self.cx-self.cbx)*self.x_scale)) + ', ' + \
+            str(int((self.cy-self.cby)*self.y_scale)) + \
+            ', ' + str(int(self.z)) + ')'
+        cv.putText(pic, centroid, (x+20, y+20), cv.FONT_HERSHEY_SIMPLEX,
+                   0.5, (255, 0, 0), 1, cv.LINE_AA)  # red text
+
 
 def composeEnv(obstacles):
     # Compose a json object to be used in optimizer
@@ -67,22 +75,23 @@ def composeEnv(obstacles):
     o_list = []
     for o in obstacles:
         count = 0
-        x, y, w, h = o.boundingRect
+        (x, y), (w, h), _ = o.rect
 
         # destination
         if o.type == 'goal':
-            destination = {'x': (o.cx - o.cbx)*o.x_scale, 
-                           'y': (o.cy - o.cby)*o.y_Scale, 
-                           'width': w*o.x_scale, 
-                           'height': h*o.y_scale}
+            destination = {'x': (o.cx - o.cbx)*o.x_scale / 1000,
+                           'y': (o.cy - o.cby)*o.y_Scale / 1000,
+                           'width': w*o.x_scale / 1000,
+                           'height': h*o.y_scale / 1000}
             global_dict['destination'] = destination
 
         else:
-            o_dict = {'type': o.type, 
-                      'cx': (o.cx - o.cbx)*o.x_scale, 
-                      'cy': (o.cy - o.cby)*o.y_scale, 
-                      'width': w*o.x_scale, 
-                      'height': h*o.y_scale}
+            # i know cx and cy is a better name but let me test this first
+            o_dict = {'type': o.type,
+                      'x': (o.cx - o.cbx)*o.x_scale / 1000,
+                      'y': (o.cy - o.cby)*o.y_scale / 1000,
+                      'width': w*o.x_scale / 1000,
+                      'height': h*o.y_scale / 1000}
             o_list.append(o_dict)
 
     # obstacles
@@ -92,35 +101,38 @@ def composeEnv(obstacles):
     global_dict['n_obstacles'] = len(obstacles)
 
     # bounds
-    bounds = {'x':[-0.8, 0.8], 'y':[0, 1.6], 'rotation':[0, 31.4159265359]}
+    bounds = {'x': [-0.8, 0.8], 'y': [0, 1.6], 'rotation': [0, 31.4159265359]}
     global_dict['bounds'] = bounds
 
-    # ball    
-    ball = {'radius': 0.01, 'location':[-0.6, 1.5], 'linear_velocity':[0.3, -0.1]}
+    # ball
+    ball = {'radius': 0.01,
+            'location': [-0.6, 1.5], 'linear_velocity': [0.3, -0.1]}
     global_dict['ball'] = ball
 
+    return global_dict
 
-    return global_dict 
 
-def fitRectangles(pic, visualize = False):
+def fitRectangles(pic, visualize=False):
 
     # calibrate camera first
     cam = cc.Cali_Cam()
     target_pts, image_pts = cam.extract_points(pic)
-    cb_origin, cb_diagonal, x_scale, y_scale = cam.CB_bounds(image_pts)  # will be used later to mask out the checkerboard
+    cb_origin, cb_diagonal, x_scale, y_scale = cam.CB_bounds(
+        image_pts)  # will be used later to mask out the checkerboard
 
     upper_left = np.array([cb_origin[0], cb_diagonal[1]]).astype(int)
     bottom_right = np.array([cb_diagonal[0], cb_origin[1]]).astype(int)
 
     H, intrinsic = cam.camera_params(target_pts, image_pts, pic)
-    dist_z = H[2,3]
+    dist_z = H[2, 3]
 
     # extraction
     gray = cv.cvtColor(pic, cv.COLOR_BGR2GRAY)
 
     # bw image, white is region of interest
     t = np.ceil(0.42*256)
-    ret, thresh = cv.threshold(gray, t, 1, cv.THRESH_BINARY_INV)  # assign 1 to the regions that are above t
+    # assign 1 to the regions that are above t
+    ret, thresh = cv.threshold(gray, t, 1, cv.THRESH_BINARY_INV)
 
     # remove checkerboard regions
     # thresh[upper_left[1]-padding:bottom_right[1]+padding, upper_left[0]-padding:bottom_right[0]+padding] = 0
@@ -133,11 +145,11 @@ def fitRectangles(pic, visualize = False):
     s_const = 1.0211670480549206
 
     # h channel to remove the shadow
-    h_ch = hsv[:,:,0]
+    h_ch = hsv[:, :, 0]
     h_mask = (h_ch > 0.1).astype(np.uint8)
 
     # s channel to remove dirty whiteboard
-    s_ch = hsv[:,:,1]
+    s_ch = hsv[:, :, 1]
     s_mask = (s_ch > 0.22).astype(np.uint8)
 
     mask = (thresh + h_mask + s_mask + s_mask) > 2
@@ -145,10 +157,10 @@ def fitRectangles(pic, visualize = False):
     thresh = mask*thresh
 
     # post process thresh
-    se = cv.getStructuringElement(cv.MORPH_ELLIPSE,(2, 2))
-    thresh = cv.erode(thresh, se, iterations = 5)
+    se = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2, 2))
+    thresh = cv.erode(thresh, se, iterations=5)
 
-    se = cv.getStructuringElement(cv.MORPH_ELLIPSE,(10, 10))
+    se = cv.getStructuringElement(cv.MORPH_ELLIPSE, (10, 10))
     thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, se)
 
     # median blur
@@ -164,26 +176,26 @@ def fitRectangles(pic, visualize = False):
         area = cv.contourArea(c)
         moments = cv.moments(c)
         if (area > 1000 and area < 150000):
-            obstacles.append(Obstacle(c, area, moments, x_scale, y_scale, upper_left, dist_z))
+            obstacles.append(
+                Obstacle(c, area, moments, x_scale, y_scale, upper_left, dist_z))
 
-    # if visualize:
-    #     for obs in obstacles:
-    #         obs.visualize(pic)
-    #     cv.imshow('thresh', thresh*255)
-    #     cv.imshow("Contours",pic)
-    #     cv.waitKey(0)
+    if visualize:
+        for obs in obstacles:
+            obs.visualize(pic)
+        cv.imshow('thresh', thresh*255)
+        cv.imshow("Contours", pic)
+        cv.waitKey(0)
 
-    test_data = composeEnv(obstacles)
-    return test_data
+    return composeEnv(obstacles)
 
 
 if __name__ == "__main__":
     img_names = os.listdir(TEST_IMGS_PATH)
-    imgs = bb1 = [ cv.imread(TEST_IMGS_PATH+name) for name in img_names ]
+    imgs = bb1 = [cv.imread(TEST_IMGS_PATH+name) for name in img_names]
 
     imgs = cv.imread('all2.png')
-    test_data = fitRectangles(imgs, visualize=True)
-
-    pdb.set_trace()
-
-    
+    environment = fitRectangles(imgs, visualize=True)
+    pprint.pprint(environment)
+    # r = requests.post('http://localhost:5000/getpose', json=environment)
+    # pprint.pprint(r.json())
+    pass
