14d6b35ed2e74e59c0618c65c60bda6729ea864c
msieb1 <msieb@andrew.cmu.edu>
2018-04-29 00:07:27 -0400

    changes to coord transform and reference frames

--- im2env.py
+++ im2env.py
@@ -4,7 +4,9 @@ import os
 import Cali_Cam as cc
 import requests
 import pprint
+
 import pdb
+import argparse
 
 
 TEST_IMGS_PATH = 'test_imgs/'
@@ -20,7 +22,7 @@ padding = 50
 
 
 class Obstacle:
-    def __init__(self, contour, area, moments, x_scale, y_scale, upper_left, cbx, cby, z):
+    def __init__(self, contour, area, moments, x_scale, y_scale, upper_left, cbx, cby, z, Hc2b=None):
         '''
         cbx -> the x coordinate of the origin of the checkerboard in REAL WORLD COORDINATES(mm) in CAMERA FRAME
         cby -> the y coordinate of the origin of the checkerboard in REAL WORLD COORDINATES(mm) in CAMERA FRAME
@@ -71,11 +73,8 @@ class Obstacle:
         cv.putText(pic, str(self.angle), (x, y), cv.FONT_HERSHEY_SIMPLEX,
                    0.5, (0, 255, 0), 1, cv.LINE_AA)  
         centroid = '(' + str(int(self.cx*self.x_scale+self.cbx)) + ', ' + \
-                         str(int(self.cy*self.y_scale+self.cby)) + \
+                         str(-int(self.cy*self.y_scale+self.cby)) + \
                   ', ' + str(int(self.z)) + ')'
-        # centroid = '(' + str(int((self.cx)*self.x_scale)) + ', ' + \
-        #            str(int((self.cy)*self.y_scale)) + \
-        #            ', ' + str(int(self.z)) + ')'
         cv.putText(pic, centroid, (x+20, y+20), cv.FONT_HERSHEY_SIMPLEX,
                    0.5, (255, 0, 0), 1, cv.LINE_AA)
 
@@ -99,7 +98,7 @@ def composeEnv(obstacles, row, col):
         # destination
         if o.type == 'goal':
             destination = {'x': (o.cx*o.x_scale + o.cbx) / 1000,  # x is good
-                           'y': y_bound-(o.cy*o.y_scale + o.cby) / 1000,  # y is actually bottom left, not top left
+                           'y': -(o.cy*o.y_scale + o.cby) / 1000,  # y is actually bottom left, not top left
                            'width': w*o.x_scale / 1000,
                            'height': h*o.y_scale / 1000}
             global_dict['destination'] = destination
@@ -107,15 +106,14 @@ def composeEnv(obstacles, row, col):
             # i know cx and cy is a better name but let me test this first
             o_dict = {'type': o.type,
                       'x': (o.cx*o.x_scale + o.cbx) / 1000,
-                      'y': (o.cy*o.y_scale + o.cby) / 1000,
+                      'y': -(o.cy*o.y_scale + o.cby) / 1000,
                       'width': w*o.x_scale / 1000,
                       'height': h*o.y_scale / 1000}
             o_list.append(o_dict)
 
     # bounds
-    bounds = {'x': [0, x_bound], 'y': [0, y_bound], 'rotation': [0, 31.4159265359]}
+    bounds = {'x': [-x_bound/2, x_bound/2], 'y': [-y_bound/2, y_bound/2], 'rotation': [0, 31.4159265359]}
     global_dict['bounds'] = bounds
-
     # obstacles
     global_dict['obstacles'] = o_list
 
@@ -124,10 +122,53 @@ def composeEnv(obstacles, row, col):
 
     # ball
     ball = {'radius': 0.01,
-            'location': [x_bound/3, y_bound], 'linear_velocity': [0.1, -0.05]}
+            'location': [-x_bound/4, y_bound/2], 'linear_velocity': [0.1, -0.05]}
     global_dict['ball'] = ball
 
     return global_dict
+
+
+def c2b(cam_pose, bax_pose):
+    '''
+    pass in cam_pose and bax pose as vectors: x, y, z
+    returns the transformation from camera coord to baxter coordinates
+    '''
+    cx = np.absolute(cam_pose[0])
+    cy = np.absolute(cam_pose[1])
+    cz = np.absolute(cam_pose[2])
+    bx = np.absolute(bax_pose[0])
+    by = np.absolute(bax_pose[1])
+    bz = np.absolute(bax_pose[2])
+
+    Hc2b = np.array([[0, 0, 1, (bx-cz)/1000],
+                     [-1, 0, 0, (by-cx)/1000],
+                     [0, -1, 0, (bz-cy)/1000],
+                     [0, 0, 0, 1]])
+    print('im here')
+    # pdb.set_trace()
+    return Hc2b
+
+def CamToBax(camPose):
+
+    test_rot = np.array([[0, 0, 1, 0],
+                         [-1, 0, 0, 0],
+                         [0, -1, 0, 0],
+                         [0, 0, 0, 1]])
+    #pdb.set_trace()
+    # chris = np.dot(test_rot, camPose)
+    # print(chris)
+    test_trans = np.array([[1, 0, 0, 0.2520],
+                          [0, 1, 0, 0.1133],
+                          [0, 0, 1, 0.3866],
+                          [0, 0, 0, 1]])
+
+    result = np.dot(test_trans, np.dot(test_rot, camPose))
+    print ('theirs')
+    #pdb.set_trace()
+
+    return result
+
+
 
 
 def fitRectangles(pic, visualize=False):
@@ -146,6 +187,45 @@ def fitRectangles(pic, visualize=False):
     # checkerboard origin in camera frame, real world coordinates
     cbx = H[0, 3]
     cby = H[1, 3]
+
+    # checkerboard origin, real world coordinates (mm), camera frame
+    print ('checkerboard origin x: ', cbx)
+    print ('checkerboard origin y: ', cby)
+    print ('checkerboard origin z: ', dist_z)
+
+    # camera1 = np.array([-480, -278, 710, 1])
+    # camera2 = np.array([-480+240, -278, 710, 1])
+    # H = c2b([cbx, cby, dist_z], [967.537, 519.957, 491.942])
+
+    # vec = np.array([0.967, 0.5199, 0.4919, 1])
+    # vec1 = np.array([-0.0897096909, -0.013427036, 0.715, 1])
+    # vec1 = np.array([0.970, 0.203, 0.4, 1])
+    #vec = np.array([0.076022, ])
+    # test_rot = np.array([[0, -1, 0, 0],
+    #                      [0, 0, -1, 0],
+    #                      [1, 0, 0, 0],
+    #                      [0, 0, 0, 1]])
+    # chris = np.dot(test_rot, vec1)
+    # print(chris)
+    # test_trans = np.array([[1, 0, 0, 0.967-chris[0]],
+    #                       [0, 1, 0, 0.5199-chris[1]],
+    #                       [0, 0, 1,0.4919-chris[2]],
+    #                       [0, 0, 0, 1]])
+    # test_rot = np.array([[0, 0, 1, 0],
+    #                      [-1, 0, 0, 0],
+    #                      [0, -1, 0, 0],
+    #                      [0, 0, 0, 1]])
+    # chris = np.dot(test_rot, vec1)
+    # print(chris)
+    # test_trans = np.array([[1, 0, 0, 0.967-chris[0]],
+    #                       [0, 1, 0, 0.203-chris[1]],
+    #                       [0, 0, 1,0.4-chris[2]],
+    #                       [0, 0, 0, 1]])
+ 
+    # # # test = np.linalg.inv(test)
+    
+    # chris2 = np.dot(test_trans, chris)
+    # print (chris2)
 
     # extraction
     gray = cv.cvtColor(pic, cv.COLOR_BGR2GRAY)
@@ -200,7 +280,6 @@ def fitRectangles(pic, visualize=False):
     if visualize:
         for obs in obstacles:
             obs.visualize(pic)
-            # print ('type: ', obs.type, ' solidity: ', obs.solidity, ' angle: ', obs.angle)
         cv.imshow('thresh', thresh*255)
         cv.imshow("Contours", pic)
         cv.waitKey(0)
@@ -212,12 +291,31 @@ def fitRectangles(pic, visualize=False):
 
 
 if __name__ == "__main__":
-    img_names = os.listdir(TEST_IMGS_PATH)
-
+    # img_names = os.listdir(TEST_IMGS_PATH)
     # imgs = bb1 = [cv.imread(TEST_IMGS_PATH+name) for name in img_names]
-    imgs = cv.imread('all2.png')
-    environment = fitRectangles(imgs[0], visualize=True)
+
+    imgs = cv.imread('div_test.png')
+    environment = fitRectangles(imgs, visualize=True)
     pprint.pprint(environment)
-    # r = requests.post('http://localhost:5000/getpose', json=environment)
-    # pprint.pprint(r.json())
+
+    init_x = environment['obstacles'][0]['x']  #test
+    init_y = environment['obstacles'][0]['y']  #test
+    init = [init_x, init_y,  0.715, 1]
+
+    r = requests.post('http://localhost:5000/getpose', json=environment)
+    pprint.pprint(r.json())
+
+
+    final_x = r.json()['obstacles'][0]['x']  #test
+    final_y = r.json()['obstacles'][0]['y']  #test
+    final =  [final_x, final_y,  0.715, 1]
+    rot_final = r.json()['obstacles'][0]['rotation']
+
+    init = CamToBax(init)
+    final = CamToBax(final)
+
+    print("Initial Position of Obstacle 0", init)
+    print("Final Position of Obstacle 0",final)
+
+    pdb.set_trace()
     pass
